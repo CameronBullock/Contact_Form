34f743de2701a9c238baba876cae95c44be808c1
Cameron Bullock <bullockcameron@gmail.com>
2017-07-11 16:49:10 -0600

    basic input styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Input from './Input';
 
-import {Form, Header,Textarea, H1, H4} from './styles';
+import {Form, Header, Input, Page, H1, H4} from './styles';
 
 class App extends Component {
 
   render() {
     return (
       <div className="App">
-        <Header>
-          <H1>Contact Us</H1>
-          <H4>Don't hesitate to get a hold of us!</H4>
-        </Header>
-          <Form>
-            <Input placeholder="Name" />
-            <Input placeholder="Email" />
-            <Textarea />
-          </Form>
+        <Page>
+          <Header>
+            <img src="../../public/lock.png" alt=""/>
+          </Header>
+            <Form>
+              <Input placeholder="Username" />
+              <Input type="password" placeholder="Password" />
+            </Form>
+          </Page>
       </div>
     );
   }
--- src/styles.js
+++ src/styles.js
@@ -1,12 +1,33 @@
 import { css } from 'glamor';
 import glamorous from 'glamorous';
 
-export const Form = glamorous.form({
-  background: 'lightgray',
-  height: 200,
+export const Page = glamorous.section({
+  height: '100vh',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  background: '#313131',
 });
 
-export const Textarea = glamorous.textarea({});
+export const Form = glamorous.form({
+  width: 400,
+  flexDirection: 'column',
+  display: 'flex',
+});
+
 export const Header = glamorous.section({});
+export const Input = glamorous.input({
+  padding: '20px 10px',
+  margin: '5px 0',
+  fontSize: 18,
+  textTransform: 'uppercase',
+  border: '1px solid #fff',
+
+
+
+  '::-webkit-input-placeholder': {
+    color: 'lightgray',
+  }
+});
 export const H1 = glamorous.h1({});
 export const H4 = glamorous.h4({});
